bc69d6d9ca165fe1eb768e59d154d72d112f469a
yildiztugba <okra.yildiz@gmail.com>
2023-03-09 14:12:27 +0100

    inputs

--- memegenerator/src/components/Meme.js
+++ memegenerator/src/components/Meme.js
@@ -2,9 +2,21 @@ import React from "react"
 
 export default function Meme(){
     return(
-        <>
+        <div>
+        <input
+            type="text"
+            placeholder="Top text"
         
+        />
+         <input
+            type="text"
+            placeholder="Bottom text"
         
-        </>
+        />
+       <button>
+           Get a new meme image
+       </button>
+        
+        </div>
     )
 }
