25ad2258158b2f45779c3298cd64eaa79bd44658
yildiztugba <okra.yildiz@gmail.com>
2023-03-10 09:37:17 +0100

    removed logo

--- memegenerator/src/components/Header.js
+++ memegenerator/src/components/Header.js
@@ -3,10 +3,6 @@ import React from "react"
 export default function Header(){
     return(
         <header className="header">
-            <img
-                className="header--image"
-                src=".../public/logo.png"
-            />
             <h2 className="header--title">Meme Generator</h2>
             <h3 className="header--project">React Course - Project 3</h3>
         </header>
